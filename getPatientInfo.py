015d463c0b7f011bdf0f2408d9f153e8ad8d327b
TamirTheProgram <120800855+TamirTheProgram@users.noreply.github.com>
2023-12-20 19:28:07 +0200

    Update getPatientInfo.py

--- getPatientInfo.py
+++ getPatientInfo.py
@@ -21,7 +21,7 @@ def updatepatientInfo(patientInfo,firstName, lastName, Age, bgillnessWindow):
     patientInfo.update({'lastName': lastName.capitalize()})
     patientInfo.update({'Age': Age})
     bgillnessWindow.destroy()
-    updateDB("", "", patientInfo, "", "patientinfo")
+    updateDB("", "", patientInfo, "", "","patientinfo")
 
 def deleteAllergies(patientInfo, window):
     patientInfo.update({'Allergies': []})
